9d0af32ee5d753d783c3c9f5a56fb6dda1723849
malahim-mar <armalahim01@gmail.com>
2025-10-24 22:16:25 +0500

    AddNote in NoteController

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -10,8 +10,17 @@ export const getAllNotes = async (req, res) => {
   }
 };
 
-export const createNote = (req, res) => {
-  res.status(201).send("Your Create Note Successfully");
+export const createNote = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const newNote = new Note({ title, content });
+
+    await newNote.save();
+    res.status(201).send("Your Create Note Successfully");
+  } catch (error) {
+    console.error("Error On createNote Method", error);
+    res.status(500).json({ message: "Server Error" });
+  }
 };
 
 export const updateNote = (req, res) => {
