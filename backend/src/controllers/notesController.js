de27cd277e132a42919f14b3e9b4ef6713a64a45
malahim-mar <armalahim01@gmail.com>
2025-10-26 00:30:17 +0500

    Done Update & Deleted Controller

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -23,9 +23,35 @@ export const createNote = async (req, res) => {
   }
 };
 
-export const updateNote = (req, res) => {
+export const updateNote = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const UpdatedNote = await Note.findByIdAndUpdate(
+      req.params.id,
+      { title, content },
+      { new: true }
+    );
+    if (!UpdatedNote) {
+      return res.status(404).json({ message: "Note Not Found" });
+    }
+    res.status(200).json(UpdatedNote);
+  } catch (error) {
+    console.error("Error On updateNote Method", error);
+    res.status(500).json({ message: "Server Error" });
+  }
   res.status(200).send("Your Note Update Successfully");
 };
-export const deleteNote = (req, res) => {
+export const deleteNote = async (req, res) => {
+  try {
+    const deletedNote = await Note.findByIdAndDelete(req.params.id);
+    if (!deletedNote) {
+      res.status(404).json({ message: "Note Not Found" });
+    }
+    res.status(200).json({ message: "Note Deleted Successfully" });
+  } catch (error) {
+    console.error("Error On deleteNote Method", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+
   res.status(200).send("Your Delete Note Successfully");
 };
