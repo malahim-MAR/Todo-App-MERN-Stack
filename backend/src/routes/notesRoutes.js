e93c17f8220a59bd87d5e3410f4b7fcd70736bcc
malahim-mar <armalahim01@gmail.com>
2025-11-13 01:48:06 +0500

    Notes Succesfully Fetch on Edit Page

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -10,7 +10,7 @@ import {
 const router = express.Router();
 
 router.get("/", getAllNotes);
-router.put("/:id", getNoteById);
+router.get("/:id", getNoteById);
 router.post("/", createNote);
 router.put("/:id", updateNote);
 router.delete("/:id", deleteNote);
--- frontend/src/Pages/NoteDetailPage.jsx
+++ frontend/src/Pages/NoteDetailPage.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
 const NoteDetailPage = () => {
-  return (
-    <div>NoteDetailPage</div>
-  )
-}
+  const { id } = useParams();
+  const [noteDetail, setNoteDetail] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [newSaveTitle, setNewSaveTitle] = useState("");
+  const [newSaveContent, setNewSaveContent] = useState("");
 
-export default NoteDetailPage
+  useEffect(() => {
+    const fetchNote = async () => {
+      try {
+        setLoading(true);
+        const res = await axios.get(`http://localhost:5001/api/notes/${id}`);
+        setNoteDetail(res.data);
+        setNewSaveTitle(res.data.title || "");
+        setNewSaveContent(res.data.content || "");
+      } catch (error) {
+        console.log("error fetching Note", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNote();
+  }, [id]);
+
+  if (loading) return <p className="text-forest-800">Loading...</p>;
+  if (!noteDetail) return <p className="text-red-600">Note not found.</p>;
+
+  return (
+    <div className="p-6 max-w-lg mx-auto bg-forest-100 rounded-lg shadow-md">
+      <form className="flex flex-col gap-4">
+        <div className="form-control w-full">
+          <label className="label">
+            <span className="label-text font-semibold">Title:</span>
+          </label>
+          <input
+            type="text"
+            className="input input-bordered input-forest w-full"
+            value={newSaveTitle}
+            onChange={(e) => setNewSaveTitle(e.target.value)}
+          />
+        </div>
+
+        <div className="form-control w-full">
+          <label className="label">
+            <span className="label-text font-semibold">Content:</span>
+          </label>
+          <textarea
+            className="textarea textarea-forest w-full h-48 resize-none"
+            value={newSaveContent}
+            onChange={(e) => setNewSaveContent(e.target.value)}
+          />
+        </div>
+        <button className="btn btn-primary">Save</button>
+      </form>
+    </div>
+  );
+};
+
+export default NoteDetailPage;
